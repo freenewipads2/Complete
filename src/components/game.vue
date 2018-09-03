021742044245696b5e1fefc01d8bc4f5735af201
freenewipads2 <nikki.sollid@gmail.com>
2018-09-03 21:55:24 +0200

    first commit

---
+++ src/components/game.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="game-wrapper">
+GAME!
+</div>
+</template>
+<script>
+export default {
+  name: 'game',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+methods: {
+  test(val){
+    return "dumma dig! " + val
+  }
+}
+}
+</script>
---
+++ src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="index-wrapper">
+asdsad {{test("hej")}}
+</div>
+</template>
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+methods: {
+  test(val){
+    return "dumma dig! " + val
+  }
+}
+}
+</script>
